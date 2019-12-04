0317bf60f9e7da2a35c2a3c2e79f15280618fe86
davidhennig <davidjhennig@gmail.com>
2019-12-04 10:23:54 -0500

    finished

--- app/src/App.js
+++ app/src/App.js
@@ -4,70 +4,43 @@ import "./App.css";
 
 class App extends React.Component {
   state = {
-    followers: [],
-    followerText: ""
+    followers: []
+    // followerText: ""
   };
 
   componentDidMount() {
     console.log("cDM");
-    axios.get("https://api.github.com/users/davidhennig").then(response => {
-      this.setState({ user: response.data.message });
-      console.log(response.data);
-    });
+    axios
+      .get("https://api.github.com/users/davidhennig/followers")
+      .then(response => {
+        this.setState({ followers: response.data });
+        console.log(response.data);
+      });
 
-    window.addEventListener("resize", this.handleResize);
+    // window.addEventListener("resize", this.handleResize);
   }
-
-  componentWillUnmount() {
-    console.log("cDUN");
-    window.removeEventListener("resize", this.handleResize);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("cDUP");
-    if (prevState.followers !== this.state.followers) {
-      console.log("new followers state");
-      if (this.state.followerText === "") {
-        axios
-          .get(`https://api.github.com/users/davidhennig/followers`)
-          .then(response => {
-            this.setState({
-              followers: response.data.message,
-              followerText: ""
-            });
-            console.log(response);
-          });
-      }
-    }
-  }
-
-  handleChanges = e => {
-    this.setState({
-      followerText: e.target.value
-    });
-  };
 
   fetchFollowers = e => {
     e.preventDefault();
-    axios.get(`https://api.github.com/users/davidhennig`).then(response => {
-      this.setState({ followers: response.data.message });
-    });
+    axios
+      .get(`https://api.github.com/users/davidhennig/followers`)
+      .then(response => {
+        console.log(response.data);
+        this.setState({ followers: response.data });
+      });
   };
 
   render() {
     return (
       <div className="App">
         <h1>Hello Followers</h1>
-        <input
-          type="text"
-          value={this.state.followerText}
-          onChange={this.handleChanges}
-        />
         <button onClick={this.fetchFollowers}>Fetch followers</button>
         <div className="followers">
-          {/* {this.state.followers.map(follower => (
-            <img width="200" src={follower} key={follower} alt={follower} />
-          ))} */}
+          {this.state.followers.map(follower => (
+            <div className="pDiv" key={follower.login}>
+              <p>{follower.login}</p>
+            </div>
+          ))}
         </div>
       </div>
     );
